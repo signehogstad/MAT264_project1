7120b976621e32cfe308dc19779483fddac86a86
theresesaltskar <71634698+theresesaltskar@users.noreply.github.com>
2021-02-06 19:28:31 +0100

    Add files via upload

---
+++ euler_forward.m
@@ -0,0 +1,48 @@
+function [Y,t] = euler_forward(funcs, init_vals, h, t_start, t_end)
+% Implementation of forward (explicit) Euler method for solving a system of
+% ODEs
+% Input:  funcs      - RHSs of the equations as function handles
+%         init_vals  - Initial values for the eqns
+%         h          - Length of time step
+%         t_start    - Initial time
+%         t_end      - End time
+% Output: Y          - Matrix where each column is the iterates of one eqn
+%         t          - Time steps
+
+% Calculating number of time steps
+n = ceil((t_end - t_start)/h);
+
+% Initial values for Y and t
+Y = zeros(length(init_vals));
+
+for i = 1:length(init_vals)
+    Y(1,i) = init_vals(i);
+end
+
+t(1) = t_start;
+
+%position_funcs = funcs{1:length(init_vals)/2};
+%momentum_funcs = funcs{length(init_vals)/2:end};
+
+for i = 1:n
+    % Updating position functions
+    for k = 1:length(init_vals)/2
+        Y(i+1,k) = Y(i,k) + h*funcs{k}(Y(i,length(init_vals)/2+k));
+    end
+    
+    % Updating momentum functions
+    for k = (length(init_vals)/2+1):length(init_vals)
+        Y(i+1,k) = Y(i,k) + h*funcs{k}(Y(i,1),Y(i,2),Y(i,3),Y(i,4),Y(i,5),Y(i,6),Y(i,7),Y(i,8),Y(i,9),Y(i,10),Y(i,11),Y(i,12));
+    end
+    
+    
+    % Updating y for each function
+%     Y(i+1,1) = Y(i,1) + h*funcs{1}(Y(i,1),Y(i,2),Y(i,3),Y(i,4));
+%     Y(i+1,2) = Y(i,2) + h*funcs{2}(Y(i,1),Y(i,2),Y(i,3),Y(i,4));
+%     Y(i+1,3) = Y(i,3) + h*funcs{3}(Y(i,1),Y(i,2),Y(i,3),Y(i,4));
+%     Y(i+1,4) = Y(i,4) + h*funcs{4}(Y(i,1),Y(i,2),Y(i,3),Y(i,4));
+    % Add more lines + arguments if there are more than 2 functions
+    
+    % Updating t
+    t(i+1) =  t(i) + h;
+end
